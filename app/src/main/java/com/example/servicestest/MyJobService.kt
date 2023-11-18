d01746db684e96506fa35d55a160a84ee20aab5d
Ptitsa_Chebupitstsa <kerugeru@gmail.com>
2023-11-19 05:27:02 +0600

    9.7 JobService. Создание сервиса

---
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -0,0 +1,52 @@
+package com.example.servicestest
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyJobService : JobService() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+            jobFinished(params,true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService:$message")
+    }
+
+}
