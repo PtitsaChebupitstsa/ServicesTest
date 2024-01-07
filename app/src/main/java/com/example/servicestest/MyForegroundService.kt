a5a2895c0dbf6c6e723e5e87f5c51cd34f76482c
Ptitsa_Chebupitstsa <kerugeru@gmail.com>
2024-01-08 01:41:25 +0600

    17.3 Bound Services

--- app/src/main/java/com/example/servicestest/MyForegroundService.kt
+++ app/src/main/java/com/example/servicestest/MyForegroundService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -18,15 +19,23 @@ import kotlinx.coroutines.launch
 
 class MyForegroundService : Service() {
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private val notificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
+    private val notificationBuilder by lazy {
+        createNotificationBuilder()
+    }
+
+    var onProgressChanged:((Int)->Unit)?= null
     override fun onCreate() {
         super.onCreate()
         log("onCreate")
        createNotificationChannel()
-        startForeground(NOTIFICATION_ID,createNotification())
+        startForeground(NOTIFICATION_ID,notificationBuilder.build())
 
     }
 private fun createNotificationChannel() {
-    val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         val notificationChannel = NotificationChannel(
             CHANNEL_ID,
@@ -35,19 +44,24 @@ private fun createNotificationChannel() {
         notificationManager.createNotificationChannel(notificationChannel)
     }
 }
-    private fun createNotification()= NotificationCompat.Builder(this,CHANNEL_ID )
+    private fun createNotificationBuilder()= NotificationCompat.Builder(this,CHANNEL_ID )
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentText("123")
             .setContentTitle("e31")
-            .build()
+        .setProgress(100,0,false)
+        .setOnlyAlertOnce(true)
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
 
         coroutineScope.launch {
-            for (i in 0 until 10) {
+            for (i in 0 .. 100 step 5) {
                 delay(1000)
+                val notification = notificationBuilder.setProgress(100,i,false)
+                    .build()
+                notificationManager.notify(NOTIFICATION_ID,notification)
+                onProgressChanged?.invoke(i)
                 log("Timer $i")
             }
             stopSelf()
@@ -61,12 +75,15 @@ private fun createNotificationChannel() {
         coroutineScope.cancel()
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    override fun onBind(intent: Intent?): IBinder {
+        return LocalBinder()
     }
 
     private fun log(message: String) {
         Log.d("SERVICE_TAG", "$SERVICE_NAME:$message")
+    }
+    inner class LocalBinder:Binder(){
+        fun getService()= this@MyForegroundService
     }
 
     companion object {
